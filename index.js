5cc2c41374ede7e1f50e46a06c8fce1d74e628c8
Colter Ulrich <cju5025@gmail.com>
2021-11-27 20:59:43 -0700

    Adds Objection And User Model

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require ('express');
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
 const knex = require('knex');
 const config = require('./knexfile')['development'];
 const database = knex(config);
+
+const { Model } = require('objection');
+Model.knex(database);
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 const bcrypt = require('bcrypt');
 
-app.post('user', (request, response) => {
+const User = require('./Models/User.js');
+
+app.get('/users', (request, response) => {
+    User.query()
+        .then(users => {
+            response.json({ users })
+        })
+})
+
+app.post('/users', (request, response) => {
     const { user } = request.body
     bcrypt.hash(user.password, 12)
         .then(hashedPassword => {
@@ -22,5 +40,7 @@ app.post('user', (request, response) => {
         .then(users => {
             const user = users[0]
             response.json({ user })
-        })
-})
+        }).catch(error => response.json(error.message))
+});
+
+app.listen(4000);
